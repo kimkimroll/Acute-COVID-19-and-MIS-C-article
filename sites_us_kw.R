3521f8e781ce134ea95733a6fa0841fb651650e6
Kimberly Wong <58268664+kimkimroll@users.noreply.github.com>
2021-04-06 16:27:55 -0400

    US Map of MIS-C patients

--- sites_us_kw.R
+++ sites_us_kw.R
@@ -4,8 +4,8 @@
 #MAPS- acute covid
 library(tidyverse)
 
-#cov_dat <- read_csv("data/Site_number_by_state_8_13_2020_kw.csv")
-cov_dat <- read_csv("data/Cleaned_dataset_all_pcr_antibody_pos2020-09-04.csv")
+cov_dat <- read_csv("data/Site_number_by_state_8_13_2020_kw.csv")
+#cov_dat <- read_csv("data/Cleaned_dataset_all_pcr_antibody_pos2020-09-04.csv")
 
 cov_dat1 <- cov_dat %>% rename (site_id = 'Site #',
                                 names = 'State') %>%
@@ -17,6 +17,7 @@ cov_dat1$site_id <- as.character(cov_dat1$site_id)
 library(usmap)
 library(maptools)
 library(ggplot2)
+
 
 #downloaded shape files from census.gov
 #kshape <- readShapePoly("tl_2019_us_state/tl_2019_us_state.shp")
@@ -36,6 +37,8 @@ k_acute <- k_acute %>%
   mutate(
     site_id = str_pad(site_id, 3, pad = "0")
   )
+k_acute <- k_acute %>%
+  rename(state = "Name")
 
 library(tidyverse)
 library(here)
@@ -79,17 +82,80 @@ kmap_acute <- kmap_acute %>%
 
 
 acute_k = kmap_acute %>%
-  group_by(fips, names) %>%
+  group_by(
+    #fips, 
+    names) %>%
   summarise(
     total_case = sum(sars_pcr_pos == 1, na.rm = TRUE)
+
   )
-       
+acute_k = acute_k %>%
+      rename(state = 'names',
+             value = 'total_case')
+  
+acute_k1 = merge(acute_k, k_acute, by = c("state"), all.y = TRUE)
 
 #table_map <-  merge(fips_st_acute, acute_k, by = "fips", all.x = TRUE)     
 #write.csv(table_map, paste0("data/kmap", Sys.Date(), ".csv", sep = ""))
 
+#statebins maps----------------------------------------------------------------------------------01062021
+
+library(statebins)
+#dat <- data.frame(state=state.abb, value=0, stringsAsFactors=FALSE)
+#dat[dat$state %in% c("AL", "NM", "SD"),]$value <- 1
 
 
+  
+#acute_k$state <- as.character(acute_k$state)  
+#acute_k = acute_k[!(acute_k$state==""),] 
+#acute_k = acute_k[!(acute_k$state=="DC"),] 
+#acute_k = acute_k %>%
+#  slice(1:(n()-1))
+acute_k1$value<-as.numeric(acute_k1$value)
+#acute_k1$value<-sub("NA", "0", acute_k1$value)
+
+acute_k1 = acute_k1 %>%  mutate_if(is.numeric, ~replace(., . == 0, NA))
+acute_k1$value[is.na(acute_k1$value)] <- 0
+acute_k1$value<-as.numeric(acute_k1$value)
+
+kbin<-statebins(
+  acute_k1, 
+  value_col = "value",
+ # breaks = c('0', '40', '80', '100'),
+ breaks = c(0, 20, 40, 60, 80, 100, 120),
+ #labels = c("0-40", "0-40", "0-40", "40-80", "80-100", "100-120"),
+  name = "# of cases",
+  palette = "RdBu",
+  font_size = 3,
+  direction = -1,
+  #round = TRUE
+ ) +
+ #labs(x="2001-2007") + 
+  theme_statebins(legend_position="right")#+
+  #facet_geo(~state, grid ="us_state_grid3")
+
+
+kbin
+
+ggsave("acute_map_kim2.png", dpi = 500, height = 8, width = 11 , units = "in")
+
+
+#GEOFACET
+library(geofacet)
+kgeo<-ggplot(acute_k1, aes(x=state, y = value, fill=value))+
+  geom_col(position = position_dodge()) +
+               facet_geo(~ state)+
+  #scale_fill_brewer(palette = "GnBu")+
+  scale_y_continuous(breaks = c(20, 40, 60, 80, 100))+
+  theme_bw()+
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+
+kgeo
+ggsave("acute_map_kim1_geofacet.png", dpi = 500, height = 8, width = 11 , units = "in")
+
+##############################################################################################this works
 kmap1 <- plot_usmap(regions = "states", data = acute_k, 
                     color = "black", 
                     labels = TRUE, 
